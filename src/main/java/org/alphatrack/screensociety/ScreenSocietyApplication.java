5352296379d421259e61ea30b6e3d924f6e80294
Svetoslav <niftoriuss@gmail.com>
2026-06-22 19:30:08 +0200

    Initial commit

---
+++ src/main/java/org/alphatrack/screensociety/ScreenSocietyApplication.java
@@ -0,0 +1,13 @@
+package org.alphatrack.screensociety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenSocietyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenSocietyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/alphatrack/screensociety/ScreenSocietyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alphatrack.screensociety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenSocietyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
