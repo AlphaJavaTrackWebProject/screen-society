73e2fcf9c82cfcccfee964d8c8f7b4b825f799d3
Svetoslav <niftoriuss@gmail.com>
2026-06-25 09:48:35 +0200

    add implementation for User model,create the Role enum and add Lombok dependency

--- src/main/java/org/alphatrack/screensociety/models/User.java
+++ src/main/java/org/alphatrack/screensociety/models/User.java
@@ -1,4 +1,37 @@
 package org.alphatrack.screensociety.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.alphatrack.screensociety.models.enums.Role;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Column(name = "e_mail", nullable = false, unique = true)
+    private String email;
+
 }
---
+++ src/main/java/org/alphatrack/screensociety/models/enums/Role.java
@@ -0,0 +1,7 @@
+package org.alphatrack.screensociety.models.enums;
+
+public enum Role {
+    USER,
+    ADMIN,
+    MODERATOR
+}
