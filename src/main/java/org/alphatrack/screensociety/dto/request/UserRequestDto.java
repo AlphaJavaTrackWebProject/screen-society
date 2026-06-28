7ecf5bb091b167848800a64315a5fd5602213d9f
Aleksandar-hash <cremonawow@gmail.com>
2026-06-28 12:16:16 +0300

    implement User request and response DTO-s

--- src/main/java/org/alphatrack/screensociety/dto/request/UserRequestDto.java
+++ src/main/java/org/alphatrack/screensociety/dto/request/UserRequestDto.java
@@ -1,4 +1,39 @@
 package org.alphatrack.screensociety.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRequestDto {
+
+    @NotNull(message = "First name cannot be empty")
+    @Size(message = "The length of First name should be between 2 and 20 characters"
+            , min = 2, max = 20)
+    private String firstName;
+
+    @NotNull(message = "Last name cannot be empty")
+    @Size(message = "The length of Last name should be between 2 and 20 characters"
+            , min = 2, max = 20)
+    private String lastName;
+
+    @NotNull(message = "User name cannot be empty")
+    @Size(message = "The length of  User name should be between 2 and 20 characters"
+            , min = 2, max = 20)
+
+    private String userName;
+
+    @NotNull(message = "Email cannot be empty")
+    @Email(message = "Invalid email")
+    private String email;
+
+
+
 }
--- src/main/java/org/alphatrack/screensociety/dto/response/UserResponseDto.java
+++ src/main/java/org/alphatrack/screensociety/dto/response/UserResponseDto.java
@@ -1,4 +1,29 @@
 package org.alphatrack.screensociety.dto.response;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.alphatrack.screensociety.models.enums.Role;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponseDto {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    /*TODO discuss*/
+    private String userName;
+
+    private String email;
+
+    private Role role;
+
 }
